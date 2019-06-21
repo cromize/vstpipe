062d3f4c8a6e11054d6381253b6b806ea3446246
cromize <mikezemanek@gmail.com>
2019-06-21 17:36:36 +0200

    windows desktop audio capture works

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import sys
 import time
 import queue
 import socket
@@ -84,13 +85,29 @@ class PipeServer():
     elif command == PipeCommand.AUDIO_PROCESS_COMMAND:
       self.process()
 
-  def audio_stream_open(self, sample_rate, buffer_size):
-    self.audio_stream = pa.open(format=pyaudio.paFloat32,
-                                channels=2,
-                                rate=sample_rate,
-                                frames_per_buffer=buffer_size,
-                                output=True,
-                                stream_callback=self.get_audio_chunk)
+  def audio_stream_open(self, sample_rate, buffer_size, device_index=0, input=False):
+    if input:
+      try:
+        self.audio_stream = pa.open(format=pyaudio.paInt16,
+                                    channels=2,
+                                    rate=sample_rate,
+                                    frames_per_buffer=buffer_size,
+                                    input=True,
+                                    input_device_index=device_index,
+                                    as_loopback=True)
+                                    #stream_callback=self.get_audio_chunk)
+      except Exception as e:
+        print(e)
+        print("*** audio device doesn't support loopback")
+        sys.exit(1)
+
+    else:
+      self.audio_stream = pa.open(format=pyaudio.paFloat32,
+                                  channels=2,
+                                  rate=sample_rate,
+                                  frames_per_buffer=buffer_size,
+                                  output=True,
+                                  stream_callback=self.get_audio_chunk)
 
   def audio_stream_close(self):
     self.audio_stream.close()
@@ -103,17 +120,54 @@ class PipeServer():
 
     self.audio_stream_open(44100, self.buffer_size)
     self.audio_stream.start_stream()
+
+  def audio_device_select(self, pa):
+    if pa.get_default_input_device_info() == -1:
+      print("*** no usable device found")
+      sys.exit(1)
+
+    # print WASAPI devices
+    print("\naudio devices: ")
+    for i in range(pa.get_device_count()):
+      info = pa.get_device_info_by_index(i)
+      #if info['hostApi'] != 1:
+        #continue
+      print(f" * {info['index']}: {info['name']} ({info['defaultSampleRate']})")
+
+    userin = input("\nselect device: ")
+    if not userin.isdigit():
+      print("*** bad input")
+      sys.exit(1)
+    return int(userin)
+
+  def audio_device_read(self):
+    data = self.audio_stream.read(512)
+    print(data)
+    return data
     
 if __name__ == "__main__":
   # init pyaudio
   print("** audio output init")
+  pa = pyaudio.PyAudio()
 
   # init pipe server
   print("** pipe server init")
   pipe_server = PipeServer("127.0.0.1", 24325)
 
-  # init audio output
-  pa = pyaudio.PyAudio()
+  audio_device_id = pipe_server.audio_device_select(pa)
+  pipe_server.audio_stream_open(44100, 512, audio_device_id, input=True)
+  rec = []
+  for i in range(int(44100/512*5)):
+    rec.append(pipe_server.audio_device_read())
+
+  import wave
+  wavefile = wave.open("test.wav", 'wb')
+  wavefile.setnchannels(2)
+  wavefile.setsampwidth(pa.get_sample_size(pyaudio.paInt16))
+  wavefile.setframerate(44100)
+  wavefile.writeframes(b"".join(rec))
+  wavefile.close()
+  sys.exit(0)
 
   # run socket thread
   pipe_server.running = True
